02fedc00321f20aa4c7f863a562bc77f84fb59c3
wkdtpzld <87063105+wkdtpzld@users.noreply.github.com>
2022-08-22 19:14:14 +0900

    configureStore , createReducer

--- src/store.js
+++ src/store.js
@@ -1,36 +1,38 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createReducer } from "@reduxjs/toolkit";
 import { createAction } from "@reduxjs/toolkit";
 
 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
 
-const reducer = (state = localStorage.toDos
-                        ? JSON.parse(localStorage.toDos)
-                        : [], action) => {
-    switch(action.type) {
-        case addToDo.type:
-            localStorage.setItem
-                ("toDos", JSON.stringify(
-                    [{
-                        text: action.payload,
-                        id:Date.now()
-                    }, ...state]));
-            return JSON.parse(localStorage.toDos);  
+const reducer = createReducer(localStorage.toDos ? JSON.parse(localStorage.toDos): [], {
 
-        case deleteToDo.type:
-            const output = JSON.parse(localStorage.toDos);
-            localStorage.setItem
-                ("toDos", JSON.stringify(
-                    output.filter(toDo => toDo.id !== action.payload))
-                );
-            return JSON.parse(localStorage.toDos);
-        default:
-            return state;
+    /**
+     *  @param {action.payload === toDos.text}} state
+     *  @param {addToDo} action
+     *  state를 Mutate시에는 반드시 Return값을 넣어서는 안됨. 
+     *  Immer 라는 패키지 내에서 변경이 일어났을 경우에 아무것도 Return하지 않음.
+     *  */
+    [addToDo]: (state, action) => {
+        state.push({ text: action.payload, id: Date.now() });
+        localStorage.setItem("toDos", JSON.stringify(state));
+    },
+
+    /**
+     * filter를 사용한 Array 재구성을 하였지만 이 보다 더 좋은 방법을 알 수가없음. 어케하냐진짜?
+     * @param {action.paylaod === toDos.id} state 
+     * @param {deleteToDo} action 
+     */
+    [deleteToDo]: (state, action) => {
+        localStorage.setItem
+            ("toDos", JSON.stringify(
+                state.filter(toDo => toDo.id !== action.payload))
+            );
+        return JSON.parse(localStorage.toDos);
     }
-}
+});
 
-const store = configureStore({reducer: reducer});
+const store = configureStore({reducer});
 
 export const actionCreators =  {
     addToDo,
